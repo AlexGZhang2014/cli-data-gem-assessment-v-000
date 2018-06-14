f2113e68b205d6f6c79d163bf72fda7a09eee8cc
Alex Zhang <alexgzhang92@gmail.com>
2018-06-14 21:21:21 +0000

    automatically backed up by learn

---
+++ top-ten-video-games/lib/top/ten/video/games.rb
@@ -0,0 +1,11 @@
+require "top/ten/video/games/version"
+
+module Top
+  module Ten
+    module Video
+      module Games
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ top-ten-video-games/lib/top/ten/video/games/version.rb
@@ -0,0 +1,9 @@
+module Top
+  module Ten
+    module Video
+      module Games
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
